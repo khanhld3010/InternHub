bf00fb823f233d9594602ab735bc93875b389920
khanhld3010 <luuduckhanh3010@gmail.com>
2026-09-18 11:22:13 +0700

    create DataInitializer
    class to create database, all member need update this class when update data

--- employee-service/src/main/java/org/example/employeeservice/repository/AccountRepository.java
+++ employee-service/src/main/java/org/example/employeeservice/repository/AccountRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findByUsername(String username);
     boolean existsByUsername(String username);
+    boolean existsByUserId(Integer userId);
 }
